0307603a8e5a0772c2737c03ac17483745004232
Swayamshree Nanda <swayamshree2001@gmail.com>
2023-07-21 14:27:13 +0530

    init first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './home.css';
+import './landing.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+import Landingpage from './pages/landing';
+
+import Homepage from './pages/home';
+
+
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" element={<Landingpage />} />
+      
+      <Route path="/homepage" element={<Homepage />} />
+      
+      </Routes>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/pages/landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default Landingpage;
+
+function Landingpage(){
+      return(
+        <div>
+            <div class="overlay"></div>
+             <h1 class="candc">C & C</h1>
+             <p class="candcp">La Crema y pastel, la dulce perfección</p>
+             <button class="candcp" id="button1"> View Shop</button>
+        </div>
+
+       
+
+      )
+}
+
